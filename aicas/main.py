bc5ec3c6f8540f73fc0a827b838ca488986f1665
yl657 <yl657@duke.edu>
2020-09-13 17:59:30 -0400

    basic home and submissions

--- aicas/main.py
+++ aicas/main.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+
+@app.route('/cpapers')
+def cpapers():
+    return render_template("cpapers.html")
+
+
+@app.route('/cdemos')
+def cdemos():
+    return render_template("cdemos.html")
+
+
+@app.route('/ctutorials')
+def ctutorials():
+    return render_template("ctutorials.html")
+
+
+@app.route('/csessions')
+def csessions():
+    return render_template("csessions.html")
